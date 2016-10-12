6f6664af60f20223d453307db36f0f9373722b92
Chun Rong <crphang@gmail.com>
2016-10-12 17:26:35 +0800

    Added basic dependency manager

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='EcommerceParser',
+      version='0.01',
+      description='HTML Parser for HTML Dumps from Ecommerce Sites',
+      author='Phang Chun Rong',
+      author_email='chunrong@u.nus.edu',
+      license='MIT',
+      install_requires=[
+          'beautifulsoup4',
+      ],
+      zip_safe=False
+     )
